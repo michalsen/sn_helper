6ea2d87b354663ed3f78b2282fdb668c184cbd84
Eric Michalsen <eric.michalsen@gmail.com>
2017-08-29 07:51:11 -0500

    tighten variables

--- sn_helper.php
+++ sn_helper.php
@@ -16,7 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 wp_register_script( 'sn_helper', plugins_url() . '/sn_helper/assets/js/sn_helper.js' );
   $variable_array = array(
     'sn_path' => __(plugins_url()),
-    'sn_map_apitoken' => __('pk.eyJ1IjoibWljaGFsc2VuIiwiYSI6ImNqNW1pbXBtdDJvdG0yd2pqbjgyb2lyaGwifQ.j6eFIwG1x2DxwhyLrTLyjg'),
   );
 wp_localize_script( 'sn_helper', 'snVars', $variable_array );
 wp_enqueue_script( 'sn_helper' );
